dbcb0487890f3d710932206571f448a7171cf836
@cristodca <cristodca.studio@gmail.com>
2022-06-04 22:10:04 -0500

    git ignore adjustment

--- index.ts
+++
@@ -1,6 +0,0 @@
-const maths = require('./math.ts')
-
-console.log(maths.addTwoNumbers(1, 2))
-console.log(maths.multiplyTwoNumbers(1, 2))
-console.log(maths.substractTwoNumbers(1, 2))
-console.log(maths.divideTwoNumbers(1, 2))
